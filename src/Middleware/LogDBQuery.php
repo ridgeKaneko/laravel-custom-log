e29667b55f9a9a33c98d7f8baa4ee65e27eae448
kaneko <kaneko@ubits.co.jp>
2021-10-22 15:43:15 +0900

    DBログミドルウェアバグ修正
    ・returnを追加
    ・ミドルウェア側のチャンネル指定をオプショナルに変更

--- src/Middleware/LogDBQuery.php
+++ src/Middleware/LogDBQuery.php
@@ -10,8 +10,15 @@ use Illuminate\Http\Request;
 
 class LogDBQuery
 {
-    public function handle(Request $request, Closure $next, string $channel)
+    public function handle(Request $request, Closure $next, string $channel = null)
     {
+        //ミドルウェアでチャンネル指定があった場合
+        if (!is_null($channel)) {
+            DBLog::setChannel($channel);
+        }
+
         DBLog::listenQuery();
+
+        return $next($request);
     }
 }
